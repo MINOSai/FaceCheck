f4c847e92f7c36a6d5290cfeb5d9bbe79bd840e1
Yaswant Narayan <yoyaswant@gmail.com>
2018-03-15 22:42:26 +0530

    Initial commit

---
+++ app/src/main/java/com/minosai/facecheck/Activities/Main/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.minosai.facecheck.Activities.Main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import com.minosai.facecheck.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
+    }
+}
